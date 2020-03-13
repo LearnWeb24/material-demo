de5fce8ea650addded856e9dba783df8f3ed9b90
SARITHA <learnweb24@gmail.com>
2020-03-13 14:02:49 +0800

    changes in material-demo on 13 MAr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms'
 import { IFlash } from './flash';
 
 @Component({
@@ -8,56 +9,76 @@ import { IFlash } from './flash';
 })
 
 export class AppComponent {
-  editng = false;
+  @ViewChild('flashForm', {static:true}) flashForm: NgForm;
+  //flash: IFlash;
+  editng =  false;
   editingId: number;
   flashs: IFlash[] = [{
     question: 'Question 1',
     answer: 'Answer 1',
     show: false,
     id: getRandomNumber(),
-}, {
+    }, {
     question: 'Question 2',
     answer: 'Answer 2',
     show: false,
     id: getRandomNumber(),
-}, {
+    },  {
     question: 'Question 3',
     answer: 'Answer 3',
     show: false,
     id: getRandomNumber(),
-}];
+    }];
 
-trackById(index: number, flash: IFlash) {
-  return flash.id;
+  trackById(index: number, flash: IFlash) {
+    return flash.id;
+  }
+
+  handleToggleCard(id) {
+
+    const flash = this.flashs.find((item) => item.id === id);
+    console.log(flash);
+    flash.show = ! flash.show;
+
+  }
+
+  handleDelete(id){
+    const flashId = this.flashs.findIndex( flash => flash.id === id);
+
+    this.flashs.splice(flashId,1);
+
+  }
+
+  handleEdit(id){
+
+    this.editng = true;
+    this.editingId = id;
+  }
+
+  handleRememberedChange({id, flag}){
+    const flash = this.flashs.find((flash) => flash.id ===id);
+    flash.remembered = flag;
+
+  }
+
+  handleSubmit(flash){
+
+    this.flashs.push({
+    id:getRandomNumber(),
+    ...flash,
+    })
+    this.handleClear(flash);
+  }
+  handleClear(flash) {
+  flash = {
+      question:'',
+      answer:'',
+      show:true,
+      id:0,
+  };
+  this.flashForm.reset();
 }
-
-handleToggleCard(id) {
-
-  const flash = this.flashs.find((item) => item.id === id);
-  console.log(flash);
-  flash.show = ! flash.show;
-
-}
-
-handleDelete(id){
-  const flashId = this.flashs.findIndex( flash => flash.id === id);
-
-  this.flashs.splice(flashId,1);
-
-}
-
-handleEdit(id){
-
-this.editng = true;
-this.editingId = id;
-
-
-
-}
-
-handleRememberedChange({id, flag}){
-  const flash = this.flashs.find((flash) => flash.id ===id);
-  flash.remembered = flag;
+handleReset(){
 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { FlashComponent } from './flash/flash.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FlashComponent } from './flash/flash.component';
     BrowserModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
